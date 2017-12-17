979a293a47178c168d39f735f84a7c98c53cd567
Jofre Pinheiro <jofrepinheiro@gmail.com>
2017-12-17 15:27:56 -0300

    Project Structure created.

---
+++ SISGEF/SISGEF/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SISGEF
+{
+    public class Class1
+    {
+    }
+}
---
+++ SISGEF/Sisgef.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sisgef.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
